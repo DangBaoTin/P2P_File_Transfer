fb79ce3102bd21246e67061f042ca63c1c90c30b
LeTrongDuc2510 <leducpro25102003@gmail.com>
2023-11-16 10:45:22 +0700

    merge sendMessage & receiveMessage -> Client Class

--- client_final.py
+++ client_final.py
@@ -13,37 +13,6 @@ ADDR = (SERVER, PORT)
 ADDR_P2P = (SERVER_P2P, PORT_P2P)
 BUFFER_SIZE = 4096 # send 4096 bytes each time step
 
-def sendMessage(conn, msg):
-    message = msg.encode(FORMAT) # encode the message
-    msg_length = len(message) # get the message length
-    send_length = str(msg_length).encode(FORMAT) # encode the message length
-    padded_send_length = send_length + b' ' * (HEADER - len(send_length)) # pad the message length
-    conn.send(padded_send_length) # send the message length
-    conn.send(message) # send the message
-
-def receiveMessage(conn):
-    message = ""
-    msg_length = conn.recv(HEADER).decode(FORMAT)
-    if msg_length:
-        msg_length = int(msg_length) 
-        message = conn.recv(msg_length).decode(FORMAT)
-    return message
-
-def handlePeer(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    filename = receiveMessage(conn)
-    print(filename)
-    with open(filename, "rb") as f: 
-        while True:
-            # read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-            # file transmitting is done
-                break
-            conn.sendall(bytes_read)
-    print("File sent !")
-    conn.close()
 
 class Client:
     def __init__(self):
@@ -55,6 +24,22 @@ class Client:
         self.p2p_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.p2p_server_socket.bind(ADDR_P2P)
 
+    def sendMessage(self, conn, msg):
+        message = msg.encode(FORMAT) # encode the message
+        msg_length = len(message) # get the message length
+        send_length = str(msg_length).encode(FORMAT) # encode the message length
+        padded_send_length = send_length + b' ' * (HEADER - len(send_length)) # pad the message length
+        conn.send(padded_send_length) # send the message length
+        conn.send(message) # send the message
+
+    def receiveMessage(self, conn):
+        message = ""
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length) 
+            message = conn.recv(msg_length).decode(FORMAT)
+        return message
+
     def publishFile(self, type, filename, newFilename):
         ## HANDLE THE FILE NOT FOUND CASE ##
         listFile = os.listdir()
@@ -63,23 +48,23 @@ class Client:
             return
 
         ## SEND THE TYPE OF COMMAND AND THE FILE NAME ##
-        sendMessage(self.client, type)
-        sendMessage(self.client, filename)
-        sendMessage(self.client, newFilename)   
+        self.sendMessage(self.client, type)
+        self.sendMessage(self.client, filename)
+        self.sendMessage(self.client, newFilename)   
 
     def fetchFile(self, type, filename):
         ## SEND THE FILE NAME ##
-        sendMessage(self.client, type)  
-        sendMessage(self.client, filename)
+        self.sendMessage(self.client, type)  
+        self.sendMessage(self.client, filename)
 
         ## FILE NOT FOUND CASE
-        response = receiveMessage(self.client)
+        response = self.receiveMessage(self.client)
         print(response)
         if response.startswith("File not found !"):
             return
 
         ## RECEIVE THE LIST OF USERS HAVE THAT FILE ##
-        listUser = receiveMessage(self.client)
+        listUser = self.receiveMessage(self.client)
         
         ## CHOOSE THE LIST OF USERS ##
         print("Choose one user to download the file: ")
@@ -94,18 +79,17 @@ class Client:
         hostname = ast.literal_eval(listUser[choice-1])[0]
         filename = ast.literal_eval(listUser[choice-1])[1]
         ## SEND THE HOSTNAME BACK TO SERVER ##  
-        sendMessage(self.client, hostname)
+        self.sendMessage(self.client, hostname)
 
-        ip = receiveMessage(self.client)
-        port_p2p = receiveMessage(self.client)
-        print(f"port p2p int {int(port_p2p)} and original {port_p2p}")
+        ip = self.receiveMessage(self.client)
+        port_p2p = self.receiveMessage(self.client)
 
         ## ESTABLISH THE P2P CONNECTION ##
         p2p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        p2p_socket.connect((ip, int(port_p2p)))  # cần thay đổi port
+        p2p_socket.connect((ip, int(port_p2p)))  
         
         ## SEND THE FILE NAME TO THE PEER ##
-        sendMessage(p2p_socket, filename)
+        self.sendMessage(p2p_socket, filename)
 
         ## RECEIVE THE FILE ##
         bytes_read = p2p_socket.recv(BUFFER_SIZE)
@@ -123,11 +107,27 @@ class Client:
         elif command[0] == "fetch":
             self.fetchFile(command[0], command[1])
         elif command[0] == "disconnect":
-            sendMessage(self.client, command[0])
+            self.sendMessage(self.client, command[0])
             self.client.close()
             exit()
         else:
             print("Invalid command !")
+
+    def handlePeer(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
+
+        filename = self.receiveMessage(conn)
+        print(filename)
+        with open(filename, "rb") as f: 
+            while True:
+                # read the bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                # file transmitting is done
+                    break
+                conn.sendall(bytes_read)
+        print("File sent !")
+        conn.close()
 
     def handleP2PConnection(self):
         self.p2p_server_socket.listen()
@@ -135,7 +135,7 @@ class Client:
         while True:
             conn, addr = self.p2p_server_socket.accept()
             print("Connect p2p successfully !")
-            thread = threading.Thread(target=handlePeer, args=(conn, addr))
+            thread = threading.Thread(target=self.handlePeer, args=(conn, addr))
             thread.start()
 
     def start(self):
@@ -145,17 +145,17 @@ class Client:
 
         # REGISTER THE HOST NAME #
         hostname = input("Enter your hostname:")
-        sendMessage(self.client, hostname)
-        response = receiveMessage(self.client)
+        self.sendMessage(self.client, hostname)
+        response = self.receiveMessage(self.client)
         print(response)
         while response.startswith("The hostname already exist !"):
             hostname = input("Enter another name: ")
-            sendMessage(self.client, hostname)
-            response = receiveMessage(self.client)
+            self.sendMessage(self.client, hostname)
+            response = self.receiveMessage(self.client)
             
         # SEND THE PORT HOST P2P
         port_p2p = self.p2p_server_socket.getsockname()[1]
-        sendMessage(self.client, str(port_p2p))
+        self.sendMessage(self.client, str(port_p2p))
 
         while True:
             try:
